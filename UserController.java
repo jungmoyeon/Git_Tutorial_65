ece6aa15e9dd9aa04c61a850656cdf7328c8f2be
jungmo <gamemk@naver.com>
2020-08-19 12:18:25 +0900

    Add UserController.java[Update]

--- UserController.java
+++ UserController.java
@@ -10,3 +10,21 @@ public class UserController{
 	}
 
 }
+
+	// 회원추가 페이지로 이동
+	@RequestMapping("/user/insert.do")
+	public String insert(){
+		logger.info("경로 : insert");
+		return "user/insert";
+	}
+
+}
+
+	// 회원수정 페이지로 이동
+	@RequestMapping("/user/update.do")
+	public String insert(){
+		logger.info("경로 : update");
+		return "user/update";
+	}
+
+}
